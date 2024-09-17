f54011b59ea3a0a00e2b71c94dc5bc06794286a0
James Vongphasouk <james.vongpha@gmail.com>
2024-09-17 14:17:58 -0600

    static

--- scripts/main.js
+++ scripts/main.js
@@ -14,20 +14,45 @@ async function fetchStoicQuote() {
     }
 }
 
+// function to fetch and play the audio file from the server
+async function fetchMaleVoice() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/play-male-voice');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // response is mp3 file
+        const res = await response.json();
+        console.log(res.file_path);
+        console.log('button is being pressed')
+        const audio = new Audio(res.file_path);
+        audio.play();
+    } catch (error) {
+        console.error('Error fetching male audio:', error);
+    }
+}
+
+async function fetchFemaleVoice() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/play-female-voice');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // response is mp3 file
+        const res = await response.json();
+        console.log(res.file_path);
+        console.log('button is being pressed')
+        const audio = new Audio(res.file_path);
+        audio.play();
+    } catch (error) {
+        console.error('Error fetching female audio:', error);
+    }
+}
+
 // Fetch the quote when the page loads
 document.addEventListener('DOMContentLoaded', () => {
     fetchStoicQuote();
 });
 
-
-function playMaleVoice() {
-        //play the audio file from eleven labs that reads the stoicism quote
-        alert("play male voice");
-}
-
-function playFemaleVoice() {
-        //play the audio file from eleven labs that reads the stoicism quote
-        alert("play female voice");
-}
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from requests import get
 import asyncio
 
@@ -29,6 +30,9 @@ def read_from_file(file_path):
 url = 'https://stoic.tekloon.net/stoic-quote'
 
 stoic_quote = None
+male_audio = None
+female_audio = None 
+
 # Function to grab the stoic quote from the API
 async def grab_stoic_quote():
     global stoic_quote
@@ -44,19 +48,76 @@ async def grab_stoic_quote():
     except Exception as e:
         print(f"Error fetching quote: {e}")
 
-# TODO: using elevenlabs api to play the audio in both male and female voice
+# using elevenlabs api to play the audio in both male and female voice
 
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+ELEVENLABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
 client = ElevenLabs(
-    api_key=ELEVENLABS_API_KEY,
+    api_key=ELEVENLABS_API_KEY
 )
+
+
+async def grab_male_audio():
+    global male_audio
+
+    response = client.text_to_speech.convert(
+        voice_id="onwK4e9ZLuTAKqWW03F9", #british authoritative
+        optimize_streaming_latency="0",
+        output_format="mp3_22050_32",
+        text=stoic_quote["quote"],
+        voice_settings=VoiceSettings(
+            stability=0.1,
+            similarity_boost=0.3,
+            style=0.2,
+        ),
+    )
+
+    # Generating a unique file name for the output MP3 file
+    save_file_path = "male_voice.mp3"
+
+    # Writing the audio to a file
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
+
+    print(f"{save_file_path}: A new audio file was saved successfully!")
+
+    male_audio = save_file_path
+
+
+async def grab_female_audio():
+    global female_audio
+    response = client.text_to_speech.convert(
+        voice_id="pFZP5JQG7iQjIQuC4Bku", # british authoritative
+        optimize_streaming_latency="0",
+        output_format="mp3_22050_32",
+        text=stoic_quote["quote"],
+        voice_settings=VoiceSettings(
+            stability=0.1,
+            similarity_boost=0.3,
+            style=0.2,
+        ),
+    )
+
+    # output MP3 file
+    save_file_path = "female_voice.mp3"
+
+    # Writing the audio to a file
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
+
+    print(f"{save_file_path}: A new audio file was saved successfully!")
+
+    female_audio = save_file_path
 
 # startup
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # first check the file, and then either read the old quote or fetch a new one 
     # based on the 24-hour interval.
-
+    
     global stoic_quote
     if os.path.exists(file_path):
         data = read_from_file(file_path)
@@ -64,12 +125,19 @@ async def lifespan(app: FastAPI):
         if (dt.datetime.now() - last_updated) > dt.timedelta(days=1): 
             # if its been more than a day, fetch a new quote and write it to the file
             await grab_stoic_quote()
-            write_to_file(file_path, stoic_quote)
         else:
             stoic_quote = data['data']
     else:
         # first time running the server, fetch a new quote
         await grab_stoic_quote()
+        
+    # if audio files are not available, and its been more than a day, fetch them
+    if not (os.path.exists("male_voice.mp3") and os.path.exists("female_voice.mp3")):
+        last_updated = dt.datetime.strptime(data['last_updated'], "%Y-%m-%d %H:%M:%S")
+        if (dt.datetime.now() - last_updated) > dt.timedelta(days=1): 
+            await grab_male_audio()
+            await grab_female_audio()
+
     
     async def periodically_update_stoic_quote():
         while True:
@@ -102,6 +170,24 @@ async def get_stoic_quote():
     if not stoic_quote:
         return {"message": "Quote not available yet. Please try again later."}
     return stoic_quote
+
+# route to play the male voice
+@app.get("/play-male-voice")
+async def play_male_voice():
+    file_path = "male_voice.mp3"
+    if os.path.exists(file_path):
+        return {'file_path': file_path}
+    else:
+        return {"message": "Male voice not available yet. Please try again later."}
+
+# route to play the female voice
+@app.get("/play-female-voice")
+async def play_female_voice():
+    file_path = "female_voice.mp3"
+    if os.path.exists(file_path):
+        return {'file_path': file_path}
+    else:
+        return {"message": "Female voice not available yet. Please try again later."}
 
 @app.get("/")
 async def root():
